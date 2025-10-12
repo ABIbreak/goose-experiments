5e9763da0d6edde6ab8c2a5465e1714cb6b2c339
George Chen <gchen@cs.wisc.edu>
2025-10-12 01:09:28 -0500

    {T} intIter(i) {T} and {T} intIter(i) {RET f. {T} f {T}} proofs

---
+++ src/iter/intIter.v
@@ -0,0 +1,110 @@
+From sys_verif.program_proof Require Import prelude empty_ffi.
+From New.proof Require Import std.
+From New.generatedproof.sys_verif_code Require Import iterator.
+
+(* TODO: what does the boilerplate below do? *)
+Section proof.
+Context `{hG: !heapGS Σ} `{!globalsGS Σ} {go_ctx: GoContext}.
+
+#[global] Instance : IsPkgInit iterator := define_is_pkg_init True%I.
+#[global] Instance : GetIsPkgInitWf iterator := build_get_is_pkg_init_wf.
+
+(* First draft at proving iterators, or higher order functions in general *)
+
+(* Syntactically correct example
+Lemma wp_test (s: loc) (x: w32) :
+  {{{ is_pkg_init sync ∗ s ↦ x }}}
+    semacquire #s
+  {{{ (f: func.t), RET #f; {{{ True }}} #f #() {{{ RET #(); True }}} }}}.
+*)
+
+Lemma wp_intIter_0 (limit : w64) :
+  {{{ is_pkg_init iterator }}}
+    (* TODO: purpose of @! at the beginning? to disambiguate between the impl and go string? *)
+    @! iterator.intIter #limit
+  {{{ (f : func.t), RET #f; True }}}.
+Proof.
+  wp_start.
+  wp_alloc limit_l as "limit".
+  wp_pures.
+  iApply "HΦ".
+  wp_pures.
+  done.
+Qed.
+
+(* Second attempt at proving iterators, covering only safety *)
+
+Lemma wp_intIter_1 (*P Q*) (limit : w64) (yield : func.t) :
+  {{{
+    is_pkg_init iterator ∗ 
+    (* ⌜ int_lt #(W64 0) #limit = #true ⌝ ∗ *)
+    ⌜ 0 <= uint.nat limit <= 2 ^ 64 - 1 ⌝ ∗
+    ∀ i : w64, {{{ (*P*) True }}} #yield #i {{{ (b : bool), RET #b; (*Q*) True }}}
+  }}}
+    @! iterator.intIter #limit
+  {{{ (f : func.t), RET #f; 
+    {{{ True }}} #f #yield {{{ RET #(); True }}}
+  }}}.
+Proof.
+  wp_start as "[%limit_bounds #Hyield]".
+  wp_alloc limit_l as "limit".
+  iPersist "limit".
+  wp_pures.
+  iApply "HΦ".
+  wp_start.
+  wp_pures.
+  wp_alloc yield_l as "yield".
+  wp_pures.
+  wp_alloc i_l as "i".
+  wp_pures.
+  wp_store.
+  wp_pures.
+  iAssert (
+    ∃i : w64,
+    i_l ↦ i ∗
+    ⌜uint.nat i <= uint.nat limit ⌝
+    )%I with "[i]" as "Hinv".
+  { iFrame. word. }
+  wp_for "Hinv".
+  iDestruct "Hinv" as "[Hi %Hi_le]".
+  wp_load.
+  wp_pures.
+  wp_if_destruct.
+  + wp_pures.
+    wp_load.
+    wp_pures.
+    wp_load.
+    wp_apply ("Hyield").
+    iIntros.
+    destruct b.
+    - wp_pures.
+      iApply wp_for_post_do.
+      wp_pures.
+      wp_load.
+      wp_pures.
+      (* TODO: there has to be a better way :/ *)
+      replace (word.add i (W64 1)) with (W64 (uint.nat i + 1)) by word.
+      wp_store.
+      wp_pures.
+      iFrame.
+      word.
+    - wp_pures.
+      iApply wp_for_post_return.
+      wp_pures.
+      iApply "HΦ".
+      done.
+  + rewrite decide_False.
+    {
+      intros Hne.
+      apply (inj to_val) in Hne.
+      discriminate.
+    }
+    rewrite decide_True.
+    {
+      auto.
+    }
+    wp_pures.
+    iApply "HΦ".
+    done.
+Qed.
+
