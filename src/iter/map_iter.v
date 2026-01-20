c0a50270ebe559c812823b66c69fdadf45b83d4c
George Chen <gchen@cs.wisc.edu>
2026-01-20 15:04:39 -0600

    Finish wp_mapIter

--- src/iter/map_iter.v
+++ src/iter/map_iter.v
@@ -6,14 +6,102 @@ Section proof.
 Context `{hG: !heapGS Σ} `{!globalsGS Σ} {go_ctx: GoContext}.
 
 (* TODO: is it advisable to add the typeclasses for K and V into the context? *)
-Context `{Inhabited K} `{Countable K} `{!IntoVal K} `{!IntoValTyped K kt}.
-Context `{Inhabited V} `{!IntoVal V} `{!IntoValTyped V vt}.
+Context `{!Inhabited K} `{Countable K} `{!IntoVal K} `{!IntoValTyped K kt}.
+Context `{!Inhabited V} `{!IntoVal V} `{!IntoValTyped V vt}.
+
+Context `{Inhabited K} `{Inhabited V}.
 
 #[global] Instance : IsPkgInit iterator := define_is_pkg_init True%I.
 #[global] Instance : GetIsPkgInitWf iterator := build_get_is_pkg_init_wf.
 
+Lemma wp_inSlice slice (vs : list w64) (v : w64):
+  {{{
+    is_pkg_init iterator ∗
+    "#Hslice" :: own_slice slice DfracDiscarded vs
+  }}}
+    @! iterator.inSlice #slice #v
+  {{{
+    (b : bool), RET #b; ⌜ b = bool_decide (v ∈ vs) ⌝
+  }}}.
+Proof.
+  wp_start.
+  iNamed "Hpre".
+  wp_auto.
+  iDestruct (own_slice_len with "Hslice") as "[%Hlength %Hslice_len]".
+  iAssert (
+    ∃ (i : w64) (j : w64) (v' : w64),
+    "i" :: i_ptr ↦ i ∗
+    "j" :: j_ptr ↦ j ∗
+    "v" :: v_ptr ↦ v' ∗
+    "%Hi" :: ⌜ 0 <= sint.Z i <= length vs ⌝ ∗
+    "%Hv_not_In" :: ⌜ v ∉ firstn (sint.nat i) vs ⌝
+  )%I with "[i j v]" as "Hinv".
+  {
+    iExists (W64 0).
+    iFrame ; iPureIntro ; split ; simpl.
+    - word.
+    - rewrite firstn_0.
+      apply not_elem_of_nil.
+  }
+  wp_for "Hinv".
+  wp_if_destruct.
+  2: {
+    iApply "HΦ".
+    iPureIntro.
+    rewrite bool_decide_eq_false_2 ; last reflexivity.
+    replace (take _ _) with vs in Hv_not_In.
+    {
+      exact Hv_not_In.
+    }
+    symmetry.
+    apply firstn_all2.
+    word.
+  }
+  wp_pure ; first word.
+  wp_apply (wp_load_slice_elem slice i vs DfracDiscarded (vs !!! sint.nat i)) ; first word.
+  {
+    iSplitL.
+    - done.
+    - iPureIntro. 
+      apply list_lookup_lookup_total_lt.
+      word.
+  }
+  iIntros "_".
+  wp_auto.
+  wp_if_destruct.
+  {
+    wp_for_post.
+    iApply "HΦ".
+    rewrite bool_decide_eq_true_2 ; last done.
+    apply list_elem_of_lookup_total_2.
+    word.
+  }
+  wp_for_post.
+  iFrame.
+  iSplitL ; iPureIntro.
+  - word.
+  - replace (take _ _) with ((take (sint.nat i) vs) ++ [(vs !!! (sint.nat i))]).
+    2: {
+      replace (sint.nat (word.add _ _)) with (S (sint.nat i)) by word.
+      symmetry.
+      apply take_S_r.
+      apply list_lookup_lookup_total_lt.
+      word.
+    }
+    apply not_elem_of_app.
+    split.
+    + exact Hv_not_In.
+    + unfold not.
+      intros Hv_vs.
+      unfold not in n.
+      apply n.
+      symmetry.
+      apply list_elem_of_singleton.
+      exact Hv_vs.
+Qed.
+
 Definition is_mapIter (mapIter : func.t) (m : gmap K V) : iPropI Σ :=
-  ∀ (P : gmap K V → iPropI Σ) (Φ: val → iProp Σ) (yield : func.t), (
+  ∀ (P : gmap K V → iPropI Σ) (Φ : iProp Σ) (yield : func.t), (
     "HP" :: P(∅) ∗
     "#Hyield" :: □(
       ∀ (k : K) (v : V) (sm : gmap K V),
@@ -23,14 +111,14 @@ Definition is_mapIter (mapIter : func.t) (m : gmap K V) : iPropI Σ :=
           if (decide (ok = #true)) then
             P(<[ k := v ]> sm)
           else if (decide (ok = #false)) then
-            Φ (return_val #())
+            Φ
           else
             False
         }}
     ) ∗
-    "Htrace" :: (P(m) -∗ Φ execute_val)
+    "Htrace" :: (P(m) -∗ Φ)
   )%I -∗
-  WP #mapIter #yield {{ Φ }}.
+  WP #mapIter #yield {{ _, Φ }}.
 
 Axiom wp_map_for_range :
   ∀ mref (m : gmap K V) (body : func.t) dq (Φ: val → iProp Σ),
@@ -43,9 +131,25 @@ Axiom wp_map_for_range :
           let: "kv"%string := (map.get (#mref) "k"%string) in
           let: "v"%string := Fst "kv"%string in 
           #body "k"%string ("v"%string)
-      )%E {{ v, own_map mref dq m ∗ Φ v }} (* TODO: doesn't account for early termination *)
+      )%E {{ v, own_map mref dq m ∗ Φ v ∗
+        if (decide (v = execute_val)) then
+          True
+        else if (decide (v = return_val #())) then
+          True
+        else
+          False 
+      }} (* TODO: doesn't account for early termination *)
   ) -∗ 
-  WP map.for_range #mref #body {{ Φ }}.
+  WP map.for_range #mref #body {{ 
+    v, 
+    Φ v ∗
+    if (decide (v = execute_val)) then
+      True
+    else if (decide (v = return_val #())) then
+      True
+    else
+      False
+  }}.
 
 (* TODO: what amount of ownership should be given to the iterator? (mirroring concerns about slice iterator) *)
 Lemma wp_mapIter mref (m : gmap K V):
@@ -55,7 +159,7 @@ Lemma wp_mapIter mref (m : gmap K V):
   }}}
     @! iterator.mapIter #kt #vt #mref
   {{{ (f : func.t), RET #f; is_mapIter f m }}}.
-Proof.
+Proof using Inhabited0 Inhabited1 H0 H1. (* TODO: why are these necessary? *)
   wp_start.
   iNamed "Hpre".
   wp_auto.
@@ -69,7 +173,15 @@ Proof.
   wp_bind (map.for_range _ _).
   iApply (wp_wand with "[HP Htrace yield k v]").
   {
-    iApply (wp_map_for_range mref m _ DfracDiscarded Φ).
+    iApply (wp_map_for_range mref m _ DfracDiscarded 
+    (λ v, 
+      if (decide (v = execute_val)) then
+        Φ
+      else if (decide (v = return_val #())) then
+        Φ
+      else
+        ⌜ False ⌝%I
+    )).
     iSplitR ; first done.
     iIntros (keys_sl keys) "[%Hpure #Hkeys]".
     iDestruct (own_slice_len with "Hkeys") as "[%Hlength %Hslice_len]".
@@ -89,39 +201,33 @@ Proof.
     {
       iExists (default_val K), (default_val V), (W64 0), ∅.
       iFrame.
-      iSplitL.
-      {
-        iPureIntro.
-        split ; first set_solver.
-        apply map_empty_subseteq.
-      }
-      iSplitL.
-      {
-        iPureIntro.
-        replace (sint.nat (W64 0)) with 0%nat by word.
-        set_solver.
-      }
-      iSplitL.
-      {
-        iPureIntro.
-        replace (sint.nat (W64 0)) with 0%nat by word.
-        rewrite firstn_0.
-        set_solver.
-      }
-      word.
+      repeat iSplitL ; iPureIntro.
+      - set_solver.
+      - apply map_empty_subseteq.
+      - set_solver.
+      - set_solver.
+      - word.
+      - word.
     }
     wp_for "Hinv".
     wp_if_destruct.
     2: {
       iSplitR ; first done.
-      iApply "Htrace".
-      assert (size sm = size m) as Hsm_m_size by word.
-      assert (sm = m).
-      {
-        apply map_subseteq_size_eq ; [set_solver | lia].
+      iSplitL.
+      2: {
+        destruct (decide _) ; done.
       }
-      subst.
-      iFrame.
+      destruct (decide _).
+      2 : {
+        destruct (decide _) ; done.
+      }
+      iApply "Htrace".
+      enough (sm = m).
+      {
+        subst.
+        iFrame.
+      }
+      apply map_subseteq_size_eq ; [set_solver | lia].
     }
     wp_pure ; first word.
     wp_apply (wp_load_slice_elem _ _ _ _ (keys !!! sint.nat i)) ; first word.
@@ -137,15 +243,15 @@ Proof.
     iIntros "_".
     wp_auto.
     wp_bind (#yield _ _).
-    replace (m !! (keys !!! sint.nat i)) with (Some (m !!! (keys !!! sint.nat i))).
-    2 : {
-      symmetry.
+    assert (m !! (keys !!! sint.nat i) = Some (m !!! (keys !!! sint.nat i))) as Hm_lookup_Some.
+    {
       rewrite lookup_lookup_total_dom ; last done.
       rewrite <- Hkeys_m.
       rewrite elem_of_list_to_set.
       apply list_elem_of_lookup_total_2.
       word.
     }
+    rewrite Hm_lookup_Some.
     simpl.
     assert (take (S (sint.nat i)) keys = take (sint.nat i) keys ++ [keys !!! (sint.nat i)]) as Htake_S.
     {
@@ -156,6 +262,9 @@ Proof.
     (* This should be generalized into its own lemma after replace dom sm, but my attempts were unsuccessful *)
     assert (keys !!! sint.nat i ∉ dom sm) as Hkeys_not_elem_of.
     {
+      rewrite <- Hsm_dom.
+      apply not_elem_of_list_to_set.
+
       assert (NoDup (firstn (S (sint.nat i)) keys)) as Hfirstn_keys_NoDup.
       {
         pose proof (sublist_take keys (S (sint.nat i))).
@@ -164,8 +273,7 @@ Proof.
       rewrite Htake_S in Hfirstn_keys_NoDup.
       apply NoDup_ListNoDup in Hfirstn_keys_NoDup.
       apply NoDup_remove in Hfirstn_keys_NoDup as [_ Hnot_In].
-      rewrite <- Hsm_dom.
-      apply not_elem_of_list_to_set.
+
       unfold not.
       intros Hkeys_dom.
       apply Hnot_In.
@@ -178,14 +286,13 @@ Proof.
       wp_apply ("Hyield" $! _ _ sm).
       iFrame.
       iPureIntro.
-      split.
-      - done.
-      - apply insert_subseteq_l ; last set_solver.
-        apply lookup_lookup_total_dom.
-        rewrite <- Hkeys_m.
-        apply elem_of_list_to_set.
-        apply list_elem_of_lookup_total_2.
-        word.
+      split; first done.
+      apply insert_subseteq_l ; last set_solver.
+      apply lookup_lookup_total_dom.
+      rewrite <- Hkeys_m.
+      apply elem_of_list_to_set.
+      apply list_elem_of_lookup_total_2.
+      word.
     }
     iIntros (?).
     destruct (decide _).
@@ -195,22 +302,11 @@ Proof.
       wp_auto.
       wp_for_post.
       iFrame.
-      iSplitL.
-      {
-        iPureIntro.
-        split ; first apply map_empty_subseteq.
-        apply insert_subseteq_l ; last set_solver.
-        apply lookup_lookup_total_dom.
-        rewrite <- Hkeys_m.
-        apply elem_of_list_to_set.
-        apply list_elem_of_lookup_total_2.
-        word.
-      }
-      iSplitL.
-      {
-        iPureIntro.
-        replace (sint.nat (word.add _ _)) with (S (sint.nat i)) by word.
-        rewrite map_size_insert.
+      repeat iSplitL; iPureIntro.
+      - apply map_empty_subseteq.
+      - apply insert_subseteq_l ; last set_solver.
+        exact Hm_lookup_Some.
+      - rewrite map_size_insert.
         assert (sm !! (keys !!! sint.nat i) = None) as Hsm_lookup_none.
         {
           apply not_elem_of_dom.
@@ -218,33 +314,39 @@ Proof.
         }
         rewrite Hsm_lookup_none.
         word.
-      }
-      iSplitL.
-      {
-        iPureIntro.
+      - rewrite dom_insert_L.
         replace (sint.nat (word.add _ _)) with (S (sint.nat i)) by word.
-        symmetry.
-        rewrite dom_insert_L.
         rewrite Htake_S.
         set_solver.
-      }
-      word.
+      - word.
+      - word.
     }
     destruct (decide _).
     {
-      iIntros "HΦ'".
+      iIntros "HΦ".
       subst.
       wp_auto.
       wp_for_post.
       iFrame "#∗".
+      destruct (decide _) ; iFrame.
+      destruct (decide _) ; iFrame.
+      done.
     }
     iIntros "?".
     done.
   }
-  iIntros (?) "HΦ".
+  iIntros (v0) "[HΦ ?]".
+  destruct (decide _).
+  {
+    subst.
+    wp_auto.
+    done.
+  }
+  destruct (decide _) ; last done.
+  subst.
   wp_auto.
-  (* TODO: why is it stuck? *)
-Admitted.
+  done.
+Qed.
 
 (* TODO: tried to give this a proof in Perennial, countered by lack of map.for_range spec *)
 Axiom wp_map_len : ∀ l (m : gmap K V) dq,
@@ -253,10 +355,9 @@ Axiom wp_map_len : ∀ l (m : gmap K V) dq,
   {{{ (s : w64), RET #s; ⌜ sint.Z s = size m ⌝ ∗ l ↦${dq} m }}}.
 
 (* TODO: is this axiom sane? *)
-Axiom wp_interace_eq : ∀ (v1 v2 : V),
-  {{{ True }}}
-    interface.eq (#v1) (#v2)
-  {{{ (b : bool), RET #b; ⌜ b = bool_decide (v1 = v2) ⌝ }}}.
+Axiom wp_interface_eq : ∀ (v1 v2 : V),
+  True -∗
+  WP interface.eq #v1 #v2 {{ b, ⌜ b = #(bool_decide (v1 = v2)) ⌝ }}.
 
 Lemma wp_MapDeepEqual mref1 (m1 : gmap K V) mref2 (m2 : gmap K V) :
   {{{
@@ -268,7 +369,7 @@ Lemma wp_MapDeepEqual mref1 (m1 : gmap K V) mref2 (m2 : gmap K V) :
   {{{
     (b : bool), RET #b; ⌜ b = bool_decide (m1 = m2) ⌝
   }}}.
-Proof.
+Proof using Inhabited0 Inhabited1 H0 H1.
   wp_start.
   iNamed "Hpre".
   wp_auto.
@@ -283,16 +384,16 @@ Proof.
   2 : {
     iApply "HΦ".
     iPureIntro.
-    assert (m1 ≠ m2).
+    rewrite bool_decide_eq_false_2.
     {
       unfold not.
       intros Heq.
-      assert (size m1 ≠ size m2) as Hsize by word.
-      apply Hsize.
-      rewrite Heq.
-      done.
+      apply n.
+      rewrite Heq in Hs1.
+      rewrite <- Hs1 in Hs2.
+      word.
     }
-    rewrite bool_decide_eq_false_2 ; done.
+    done.
   }
   wp_apply (wp_mapIter mref1 m1) ; first done.
   iIntros (f) "HmapIter".
@@ -329,7 +430,12 @@ Proof.
             apply lookup_insert_eq.
           }
           simpl.
-          wp_apply wp_interace_eq.
+          wp_bind (interface.eq _ _).
+          iApply wp_wand.
+          {
+            iApply wp_interface_eq.
+            done.
+          }
           iIntros (b) "%Hb".
           subst.
           rewrite bool_decide_eq_true_2.
@@ -362,7 +468,12 @@ Proof.
           destruct i as [v' Hm2_lookup_k].
           rewrite Hm2_lookup_k.
           simpl.
-          wp_apply wp_interace_eq.
+          wp_bind (interface.eq _ _).
+          iApply wp_wand.
+          {
+            iApply wp_interface_eq.
+            done.
+          }
           iIntros (b) "%Hb".
           case_bool_decide.
           + subst.
